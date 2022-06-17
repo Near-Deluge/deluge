b158324696bdd6b75ce877f204f5c0accd50b696
Prince Anuragi <prince@uniris.io>
2022-06-17 18:22:38 +0530

    Added Footer with relevant links and pages

--- app/ui/src/icons/deluge.tsx
+++ app/ui/src/icons/deluge.tsx
@@ -1,17 +1,30 @@
 import React from "react";
 
 import iconImg from "../assets/imgs/logos/logo.png";
+import iconFImg from "../assets/imgs/logos/logo_full.png";
 
 const DelugeIcon: React.FC<{
   size?: number;
-}> = ({ size }) => {
+  height?: number;
+  width?: number;
+  variant?: "full" | "short";
+}> = ({ size, variant, width, height }) => {
   const fSize = size || 20;
-  return (
+  const fWidth = width || 100;
+  const fHeight = height || 30;
+  const fVariant = variant || "short";
+  return fVariant === "short" ? (
     <img
       src={iconImg}
       alt="deluge-icon"
       style={{ height: fSize, width: fSize }}
     ></img>
+  ) : (
+    <img
+      src={iconFImg}
+      alt="deluge-full-icon"
+      style={{ height: fHeight, width: fWidth }}
+    />
   );
 };
 
